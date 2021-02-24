92dad33e6ea3a5d5ac3864f6e095f5c9382fc330
Nurdaulet08 <66691108+Nurdaulet08@users.noreply.github.com>
2021-02-24 19:32:23 +0600

    Add files via upload

---
+++ lab4/panoalaljme.angular/src/app/products.ts
@@ -0,0 +1,106 @@
+export const products = [
+  {
+    id: 1,
+    name:
+      "Impecca 24 Inch LED HD TV Monitor TL2400H Energy Star Slim Design 720p, Built-in Speakers with Multiple Imputes HDMI, USB Ports, and Remote, Wall Mountable",
+    price: 800,
+    rating: "4.6/5",
+    description:
+      "AUDIO & VIDEO SPECS: 2 Built-in speaker 6W each, 4 Mode Equalizers, Effects YES, TV system ATSC, Progressive Scan YES, HD Ready YES, YPrPb resolution, 480i/p 576i/p 720i/p, PC Resolution UP to 1366.768 60HZ.",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81CfC2qQFdL._AC_SL1500_.jpg"
+  },
+  {
+    id: 2,
+    name:
+      "Hisense 40-Inch Class H4 Series LED Roku Smart TV with Alexa Compatibility (40H4F, 2020 Model)",
+    price: 699,
+    rating: "4.7/5",
+    description:
+      "Combining rich 1080p picture quality, intuitively smart Roku TV platform, built-in Wi-Fi, and innovative technologies, Hisense H4F series smart TVs are great for all your needs - streaming, cable, satellite, gaming, and free over-the-air TV",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71gzlKauNQL._AC_SL1500_.jpg"
+  },
+  {
+    id: 3,
+    name: "SAMSUNG QN32Q50RAFXZA Flat 32",
+    price: 1000,
+    rating: "4.2/5",
+    description:
+      "HD: see shades of color that reveal more detail than HDTV can deliver",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51NKhnjhpGL._AC_SL1024_.jpg"
+  },
+  {
+    id: 4,
+    name: "SAMSUNG 55-inch Class QLED Q80T Series",
+    price: 649,
+    rating: "4/5",
+    description:
+      "K UHD Direct Full Array 12X Quantum HDR 12X Smart TV with Alexa Built-in (QN55Q80TAFXZA, 2020 Model)",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61DIUfDxBtL._AC_SL1000_.jpg"
+  },
+  {
+    id: 5,
+    name: "TCL 32 3-Series 720p Roku Smart TV",
+    price: 599,
+    rating: "3.9/5",
+    description:
+      "Smart functionality delivers all your favorite content with over 500,000 movies and TV episodest",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61ficuy07aL._AC_SL1200_.jpg"
+  },
+  {
+    id: 6,
+     name: "Hisense 32-Inch 32H5500F Class H55 Series Android Smart TV with Voice Remote",
+    price: 799,
+    rating: "4,5/5",
+    description:
+      "Enjoy all the key moments in fast-paced sports, movies, and games with motion rate 120-image processing, minimizing picture lag to create a clear image without motion blur.Enjoy crisp, immersive audio from the two 6-watt speakers and DTS studio sound virtual surround sound, or wirelessly connect a compatible sound bar, headphone, or stereo components using built-in Bluetooth connectivity.",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/8161gQWMnNL._AC_SL1500_.jpg"
+  },
+  {
+    id: 7,
+    name: "VIZIO 75-inch P-Series Quantum X 4K UHD LED HDR Smart",
+    price: 799,
+    rating: "4.75/5",
+    description:
+      "Smart functionality delivers all your favorite content with over 500,000 movies and TV episodest",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91pKr6OfhxL._AC_SL1500_.jpg"
+  },
+  {
+    id: 8,
+    name: "SuperSonic SC-2412 LED Widescreen HDTV & Monitor 24",
+    price: 450,
+    rating: "3.9/5",
+    description: "television set. This will allow you to play your DVDs without any other equipment. DVD formats compatible are DVD, CD, CDR, CDRW, DVD+/- R, DVD+/- RW, VCD, and SVCD",
+     img:
+      "https://images-na.ssl-images-amazon.com/images/I/91p3rthIw%2BL._AC_SL1500_.jpg"
+  },
+  {
+    id: 9,
+    name: "Axess TVD1805-15 LED HDTV Includes AC/DC TV DVD Player",
+    price: 400,
+    rating: "3.5/5",
+    description: "Dimensions with stand: 16 x 13. 5 x 5 || Dimensions without stand: 16x 11x 2Package Contents: Axess TVD1805-15 15.6",
+    img:"https://images-na.ssl-images-amazon.com/images/I/61pVfrMoJ5L._AC_SL1080_.jpg"
+  },
+  {
+    id: 10,
+    name: "SuperSonic SC-2412 LED Widescreen HDTV & Monitor 24",
+    price: 750,
+    rating: "4.2/5",
+    description: "CRYSTAL PROCESSOR 4K: This ultra-fast processor transforms everything you watch into stunning 4K.MULTI VOICE: Smart TV with Alexa and Bixby.SMART TV POWERED BY TIZEN: Go beyond Smart TV with next-gen apps, super easy control, and a host of enhancements that elevate the TV watching experience.3D SURROUND SOUND - The captivating experience of DTS Virtual:X turns your living room into a theater. Powerful 3D surround sound comes to life by recreating sound that makes you feel like you're inside the sceneBLUETOOTH MULTI CONNECTION - Seamlessly switch between two different connected smart devicesPOWERFUL BASS - Feel the action and the beat of the music with the rich bass from the wireless subwoofer",
+     img:
+      "https://images-na.ssl-images-amazon.com/images/I/615kyTYOl8L._AC_SL1001_.jpg"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
